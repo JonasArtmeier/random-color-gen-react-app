6bb6315b4a89356286da2fbaf679144e3989e501
Jonas Artmeier <jonas-artmeier@web.de>
2020-05-12 16:22:02 +0200

    Add the input file is working now

--- src/Input.js
+++ src/Input.js
@@ -4,22 +4,45 @@ import React, { useState } from 'react';
 import { css, jsx } from '@emotion/core';
 // import randomColor from './randomColor';
 
+// handleInput = function () {};
+
 export default function Input() {
   // const color = randomColor()
+  const [color, setColor] = useState();
   return (
-    <input
-      type="text"
-      name="this is"
-      placeholder="name your color"
-      css={css`
-        align: center;
-        position: absolute;
-        width: 844px;
-        height: 64px;
-        left: 331px;
-        top: 120px;
-        background: grey;
-      `}
-    ></input>
+    <form>
+      <input
+        type="text"
+        name="this is"
+        placeholder="name your color"
+        css={css`
+          position: absolute;
+          width: 844px;
+          height: 64px;
+          left: 331px;
+          top: 120px;
+          background: ${color} !important;
+        `}
+        onChange={function onChange(event) {
+          setColor(event.target.value);
+        }}
+      ></input>
+    </form>
   );
 }
+
+// export default function Input() {
+//   return (
+//     <form onSubmit={this.handleSubmit}>
+//       <label>
+//         Color:
+//         <input
+//           type="text"
+//           value={this.state.value}
+//           onChange={this.handleChange}
+//         />{' '}
+//       </label>
+//       <input type="submit" value="Submit" />
+//     </form>
+//   );
+// }
